0d3224ef467421437b499589bd990ac494c64255
zhouyijun <1176902387@qq.com>
2021-06-07 22:50:10 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 import App from './App.vue'
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 
-
-// 引入样式
-import 'element-ui/lib/theme-chalk/index.css'
-
-import 'font-awesome/css/font-awesome.css'
 
 // 引入axios
 import axios from "axios"
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,6 @@
   <div class="content">文件内容</div>
 
   <div class="block">
-    <span class="demonstration">页数较少时的效果</span>
-    <el-pagination layout="prev, pager, next" :total="50"> </el-pagination>
-  </div>
-  <div class="block">
     <span class="demonstration">大于 7 页时的效果</span>
     <el-pagination layout="prev, pager, next" :total="1000"> </el-pagination>
   </div>
