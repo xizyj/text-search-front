ce2f269c779fe2b928378657a7be467965868a0f
zhouyijun <zhouyijun@myhexin.com>
2021-06-10 09:45:23 +0800

    update

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,12 +1,36 @@
 import axios from 'axios'
-
+interface dataResult{
+    code:number,
+    mesg:string,
+    data:any
+}
 const instance = axios.create({
     baseURL: 'http://localhost:3000',
     timeout: 10000,
 });
 
-function get(url:string):Promise{
-    return  instance.get(url)
+function get(url:string):Promise<dataResult>{
+    return new Promise((resolve,reject)=>{
+        resolve({
+            code:0,
+            mesg:'success',
+            data:{
+                pageNum:1,
+                totalPages:10,
+                fileList:[
+                    {
+                        fileName1:'test1',
+
+                    },
+                    {
+                        fileName2:'test2',
+
+                    }
+                ]
+            }   
+        })
+    })
+    // return  instance.get(url)
 }
 
 export {get}
--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -1,11 +1,11 @@
 <template>
-  搜索文件路径：<el-input v-model="path"></el-input> <br />
-  搜索关键词：<el-input v-model="keyName"></el-input><br />
-  搜索文件数量：<el-input v-model="num"></el-input><br />
+  path:<el-input v-model="path"></el-input> <br />
+  keyName:<el-input v-model="keyName"></el-input><br />
+  num:<el-input v-model="num"></el-input><br />
   <div class="list">
-    <div class="item" v-for="(item, i) in list" :key="'index' + i">
-      <div class="title">文件名</div>
-      <div class="content">文件内容</div>
+    <div class="item" v-for="(item,index) in list" :key="item+index">
+      <div class="title">{{Object.keys(item)[0]}}</div>
+      <div class="content">{{item[Object.keys(item)[0]]}}</div>
     </div>
   </div>
 
@@ -26,14 +26,20 @@ export default {
     let page = ref(1);
     let list = ref([]);
     onMounted(() => {
+      console.log('mounted')
       get(`/search?searchStr=${keyName.value}&filePath=${path.value}&page=${page.value}`).then(res=>{
         console.log(res)
+        if(res.code==0){
+          list.value=res.data.fileList
+          
+        }
       })
     });
     return {
       path,
       keyName,
       num,
+      list
     };
   },
 };
