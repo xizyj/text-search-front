a401c616603cf658e1e533e079d2a3798cc74dac
zhouyijun <zhouyijun@myhexin.com>
2021-06-11 16:13:36 +0800

    update

--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -1,7 +1,11 @@
 <template>
-  path:<el-input v-model="path"></el-input> <br />
-  keyName:<el-input v-model="keyName"></el-input><br />
-  num:<el-input v-model="num"></el-input><br />
+  path:
+  <el-input v-model="path"></el-input>
+  <br />keyName:
+  <el-input v-model="keyName"></el-input>
+  <br />num:
+  <el-input v-model="num"></el-input>
+  <br />
   <div class="list">
     <div class="item" v-for="(item,index) in list" :key="item+index">
       <div class="title">{{Object.keys(item)[0]}}</div>
@@ -11,7 +15,7 @@
 
   <div class="block">
     <span class="demonstration">大于 7 页时的效果</span>
-    <el-pagination layout="prev, pager, next" :total="1000"> </el-pagination>
+    <el-pagination @current-page="changePage" layout="prev, pager, next" :total="1000"></el-pagination>
   </div>
 </template>
 
@@ -25,15 +29,22 @@ export default {
     let num = ref(1);
     let page = ref(1);
     let list = ref([]);
-    onMounted(() => {
-      console.log('mounted')
-      get(`/search?searchStr=${keyName.value}&filePath=${path.value}&page=${page.value}`).then(res=>{
-        console.log(res)
-        if(res.code==0){
-          list.value=res.data.fileList
-          
+    const changePage = (v: number) => {
+      page.value = v;
+    };
+
+    const getData = () => {
+      get(
+        `/search?searchStr=${keyName.value}&filePath=${path.value}&page=${page.value}`
+      ).then(res => {
+        if (res.code == 0) {
+          list.value = res.data.fileList;
         }
-      })
+      });
+    };
+
+    onMounted(() => {
+      getData();
     });
     return {
       path,
@@ -41,7 +52,7 @@ export default {
       num,
       list
     };
-  },
+  }
 };
 </script>
 
